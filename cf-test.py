f4215cad3a738f23f39b4e678048e7761f7ba8c5
tpbrisco <tpbrisco+gh@gmail.com>
2016-06-11 17:31:50 -0400

    initial stab using "requests". found from an example on stackoverflow  http://stackoverflow.com/questions/34717166/how-to-list-all-apps-in-cloudfoundry-using-python

---
+++ cf-test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+# stolen from
+# http://stackoverflow.com/questions/34717166/how-to-list-all-apps-in-cloudfoundry-using-python
+
+import requests
+
+oauthTokenResponse = requests.post(
+    'https://login.run.pivotal.io/oauth/token?grant_type=password&client_id=cf',
+    data={'username': 'tpbrisco+pivotal@gmail.com',
+          'password': 's#VoU&UP7m#^',
+          'client_id': 'cf'},
+    auth=('cf',''))
+authorization = oauthTokenResponse.json()['token_type'] + ' ' + oauthTokenResponse.json()['access_token']
+
+appsResponse = requests.get('https://api.run.pivotal.io/v2/apps',
+                            headers={'Accept': 'application/json', 'Content-Type': 'application/json',
+                                     'Authorization': authorization}
+                            )
+
+print appsResponse.json()
+

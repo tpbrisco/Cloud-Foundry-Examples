9f587c1a9c2040b03d6e2d41fb0675d157ff90fa
tpbrisco <tpbrisco+gh@gmail.com>
2016-06-12 19:50:13 -0400

    Parameterize username/password

--- cf-test.py
+++ cf-test.py
@@ -3,13 +3,17 @@
 # stolen from
 # http://stackoverflow.com/questions/34717166/how-to-list-all-apps-in-cloudfoundry-using-python
 
+import os, sys
+import getpass
 import requests
 import json
 
+username = raw_input('username:')
+password = getpass.getpass('password:')
 oauthTokenResponse = requests.post(
     'https://login.run.pivotal.io/oauth/token?grant_type=password&client_id=cf',
-    data={'username': 'tpbrisco+pivotal@gmail.com',
-          'password': 's#VoU&UP7m#^',
+    data={'username': username,
+          'password': password,
           'client_id': 'cf'},
     auth=('cf',''))
 authorization = oauthTokenResponse.json()['token_type'] + ' ' + oauthTokenResponse.json()['access_token']

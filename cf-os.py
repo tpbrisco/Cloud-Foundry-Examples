695c98de8fa7e072a7a23223e64bacc1b0cd4e24
tpbrisco <tpbrisco+gh@gmail.com>
2016-06-12 16:08:30 -0400

    Messing around with parsing ~/.cf/config.json

---
+++ cf-os.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+# stolen from
+# http://stackoverflow.com/questions/34717166/how-to-list-all-apps-in-cloudfoundry-using-python
+
+import os, sys
+import requests
+import json
+
+oauthTokenResponse = requests.post(
+    'https://login.run.pivotal.io/oauth/token?grant_type=password&client_id=cf',
+    data={'username': 'tpbrisco+pivotal@gmail.com',
+          'password': 's#VoU&UP7m#^',
+          'client_id': 'cf'},
+    auth=('cf',''))
+authorization = oauthTokenResponse.json()['token_type'] + ' ' + oauthTokenResponse.json()['access_token']
+
+appsResponse = requests.get('https://api.run.pivotal.io/v2/apps',
+                            headers={'Accept': 'application/json', 'Content-Type': 'application/json',
+                                     'Authorization': authorization}
+                            )
+# print oauthTokenResponse.json()['token_type'], oauthTokenResponse.json()['access_token']
+print json.dumps(appsResponse.json(), indent=2, separators={',',':'})
+
+with open(os.path.expanduser("~/.cf/config.json"), "r") as f:
+    current_login = json.load(f)
+print "org=",current_login['OrganizationFields']['Name']
+print "space=",current_login['SpaceFields']['Name']

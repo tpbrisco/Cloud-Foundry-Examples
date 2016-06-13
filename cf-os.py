e0970bbb8e27388692c5a3663540a3660a7149fe
tpbrisco <tpbrisco+gh@gmail.com>
2016-06-13 18:24:35 -0400

    Use either cached credentials (from CF CLI), or prompt for them from the user.

--- cf-os.py
+++ cf-os.py
@@ -8,19 +8,31 @@ import getpass
 import requests
 import json
 
-username = raw_input('username:')
-password = getpass.getpass('password:')
+current_login = {}
 
-oauthTokenResponse = requests.post(
-    'https://login.run.pivotal.io/oauth/token?grant_type=password&client_id=cf',
-    data={'username': username,
-          'password': password,
-          'client_id': 'cf'},
-    auth=('cf',''))
-if not oauthTokenResponse.ok:
-    print "Error in authentication: ",oauthTokenResponse.text
-    sys.exit(1)
-authorization = oauthTokenResponse.json()['token_type'] + ' ' + oauthTokenResponse.json()['access_token']
+if os.path.isfile(os.path.expanduser("~/.cf/config.json")):
+    # use existing cached credentials
+    with open(os.path.expanduser("~/.cf/config.json"), "r") as f:
+        current_login = json.load(f)
+    authorization = current_login['AccessToken']
+    print "Using existing credentials:\"" + authorization + "\""
+else:
+    # use new credentials
+    username = raw_input('username:')
+    password = getpass.getpass('password:')
+
+    oauthTokenResponse = requests.post(
+        'https://login.run.pivotal.io/oauth/token?grant_type=password&client_id=cf',
+        data={'username': username,
+              'password': password,
+              'client_id': 'cf'},
+        auth=('cf',''))
+    if not oauthTokenResponse.ok:
+        print "Error in authentication: ",oauthTokenResponse.text
+        sys.exit(1)
+    
+    authorization = oauthTokenResponse.json()['token_type'] + ' ' + oauthTokenResponse.json()['access_token']
+    print "authorization:\"" + authorization + "\""
 
 appsResponse = requests.get('https://api.run.pivotal.io/v2/apps',
                             headers={'Accept': 'application/json', 'Content-Type': 'application/json',
@@ -28,8 +40,3 @@ appsResponse = requests.get('https://api.run.pivotal.io/v2/apps',
                             )
 # print oauthTokenResponse.json()['token_type'], oauthTokenResponse.json()['access_token']
 print json.dumps(appsResponse.json(), indent=2, separators={',',':'})
-
-with open(os.path.expanduser("~/.cf/config.json"), "r") as f:
-    current_login = json.load(f)
-print "org=",current_login['OrganizationFields']['Name']
-print "space=",current_login['SpaceFields']['Name']

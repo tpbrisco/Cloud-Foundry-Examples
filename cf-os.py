9a945b0e60c3973bc5439d39a72c39318db00a18
tpbrisco <tpbrisco+gh@gmail.com>
2016-06-13 06:49:05 -0400

    adding in error handling

--- cf-os.py
+++ cf-os.py
@@ -4,15 +4,22 @@
 # http://stackoverflow.com/questions/34717166/how-to-list-all-apps-in-cloudfoundry-using-python
 
 import os, sys
+import getpass
 import requests
 import json
 
+username = raw_input('username:')
+password = getpass.getpass('password:')
+
 oauthTokenResponse = requests.post(
     'https://login.run.pivotal.io/oauth/token?grant_type=password&client_id=cf',
-    data={'username': 'tpbrisco+pivotal@gmail.com',
-          'password': 's#VoU&UP7m#^',
+    data={'username': username,
+          'password': password,
           'client_id': 'cf'},
     auth=('cf',''))
+if not oauthTokenResponse.ok:
+    print "Error in authentication: ",oauthTokenResponse.text
+    sys.exit(1)
 authorization = oauthTokenResponse.json()['token_type'] + ' ' + oauthTokenResponse.json()['access_token']
 
 appsResponse = requests.get('https://api.run.pivotal.io/v2/apps',

84df878ec9191f5350889bd75476c569b7c254f2
tpbrisco <tpbrisco+gh@gmail.com>
2016-06-16 21:57:02 -0400

    Modularize the calls for a cloud foundry login, or refresh of the cloud foundry cached credentials.

--- cf-os.py
+++ cf-os.py
@@ -8,33 +8,10 @@ import getpass
 import requests
 import json
 
-current_login = {}
-
-if os.path.isfile(os.path.expanduser("~/.cf/config.json")):
-    # use existing cached credentials
-    with open(os.path.expanduser("~/.cf/config.json"), "r") as f:
-        current_login = json.load(f)
-    # print json.dumps(current_login, indent=2)
-    authorization = current_login['AccessToken']
-    # refresh the token
-    oauthTokenResponse = requests.post(
-        'https://login.run.pivotal.io/oauth/token?grant_type=refresh_token&client_id=cf',
-        data={'refresh_token':  current_login['RefreshToken'],
-              'client_id': 'cf',
-              'Authorization': authorization},
-        auth=('cf', ''))
-    if not oauthTokenResponse.ok:
-        print "Error in token refresh:", oauthTokenResponse.text
-        sys.exit(1)
-
-    # print json.dumps(oauthTokenResponse.json(), indent=2)
-    authorization = oauthTokenResponse.json()['token_type'] + ' ' + oauthTokenResponse.json()['access_token']
-    # print "refreshed auth:\"" + authorization + "\""
-else:
-    # use new credentials
+# attempt a login; get username/password
+def cf_login() :
     username = raw_input('username:')
     password = getpass.getpass('password:')
-
     oauthTokenResponse = requests.post(
         'https://login.run.pivotal.io/oauth/token?grant_type=password&client_id=cf',
         data={'username': username,
@@ -42,19 +19,59 @@ else:
               'client_id': 'cf'},
         auth=('cf',''))
     if not oauthTokenResponse.ok:
-        print "Error in authentication: ",oauthTokenResponse.text
-        sys.exit(1)
-    
+        print "Error in authentication: ",oauthTokenResponse.json()['error_description']
+        return {}
     authorization = oauthTokenResponse.json()['token_type'] + ' ' + oauthTokenResponse.json()['access_token']
-    print "authorization:\"" + authorization + "\""
+    # print "authorization:\"" + authorization + "\""
+    return oauthTokenResponse.json()
+
+# attempt to refresh an existing token - expect "refresh token"
+def cf_refresh(r_token):
+    oauthTokenResponse = requests.post(
+        'https://login.run.pivotal.io/oauth/token?grant_type=refresh_token&client_id=cf',
+        data = {'refresh_token':  r_token, 'client_id': 'cf'},
+        auth=('cf', ''))
+    if not oauthTokenResponse.ok:
+        print "Error in token refresh:", oauthTokenResponse.json()['error_description']
+        return {}
+    return oauthTokenResponse.json()
+
+# initialize to "no access key"
+access_key = ''
+
+# try to refresh any existing credentials
+if os.path.isfile(os.path.expanduser("~/.cf/config.json")):
+    # use existing cached credentials
+    with open(os.path.expanduser("~/.cf/config.json"), "r") as f:
+        current_login = json.load(f)
+    refresh_json = cf_refresh(current_login['RefreshToken'])
+    # print json.dumps(current_login, indent=2)
+    if len(refresh_json):
+        access_key = refresh_json['token_type'] + ' ' + refresh_json['access_token']
+    else:
+        print "Credentials refresh failed"
+
+# if a refresh was unsuccessful, revert to username/password
+if access_key == '':
+    # use new credentials from scratch
+    login_json = cf_login()
+    if len(login_json):
+        access_key = login_json['token_type'] + ' ' + login_json['access_token']
+    else:
+        print "Login failed"
+    # print "authorization:\"" + access_key + "\""
+
+if access_key == '':
+    print "All access methods failed.  Cannot connect"
+    sys.exit(2)
+else:
+    print "Authorization permitted, getting list of apps"
 
 appsResponse = requests.get('https://api.run.pivotal.io/v2/apps',
                             headers={'Accept': 'application/json', 'Content-Type': 'application/json',
-                                     'Authorization': authorization}
+                                     'Authorization': access_key}
                             )
-# print oauthTokenResponse.json()['token_type'], oauthTokenResponse.json()['access_token']
 print json.dumps(appsResponse.json(), indent=2)
-
 
 #
 resources = appsResponse.json()['resources']
@@ -62,7 +79,7 @@ for r in resources:
     route = r['entity']['route_mappings_url']
     print "Entity Route",route
     r = requests.get('http://api.run.pivotal.io' + route,
-                       headers={'client_id': 'cf', 'Authorization': authorization})
+                       headers={'client_id': 'cf', 'Authorization': access_key})
     if r.ok:
         print json.dumps(r.json(), indent=2)
     else:

3fce22ffd47555e9ac1008ab1db5b7e51207d472
tpbrisco <tpbrisco+gh@gmail.com>
2018-03-26 23:49:27 -0400

    coerce port type to int

--- sched_test/scheduled.py
+++ sched_test/scheduled.py
@@ -152,5 +152,4 @@ atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     port = os.getenv("PORT", default=5000)
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(host='0.0.0.0', port=int(port))
